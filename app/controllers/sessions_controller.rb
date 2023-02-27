8fda2b3ba42daea9e0926018ca493c0ab673485a
YANGHONG-KELLOGG <yang.hong@kellogg.northwestern.edu>
2023-02-27 06:12:35 +0000

    step1 and 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,43 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
+
   def create
-    # TODO: authenticate user
+    @user = User.find_by({"email"=>params["email"]})
+    
+    if @user
+        # if user exists
+        if BCrypt::Password.new(@user["password"]) == params["password"]
+        # not encrypted -- cookies["user_id"] =@user["id"]
+        # below is encripted 
+        # session is the encrupted cookies
+        session["user_id"] =@user["id"]
+            # send them to companies
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        redirect_to "/posts"
+        
+        else
+        #otherwise, back to login
+        flash["notice"] = "Nope"
+        redirect_to "/login"
+        end
+    else
+    #otherwise, back to login
+    flash["notice"] = "Nope"
+    redirect_to "/login"
+    end 
+end 
+
+
+def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
+
+
+
+
+
 end
