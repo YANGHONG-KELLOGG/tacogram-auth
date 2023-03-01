0ff50ee444092ca8feb7c641f9d3e48b47994f75
YANGHONG-KELLOGG <yang.hong@kellogg.northwestern.edu>
2023-03-01 02:39:15 +0000

    add no duplicate users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,28 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
-    @user["email"] = params["user"]["email"]
-    @user["password"] = BCrypt::Password.create(params["user"]["password"])
-    @user.save
-    redirect_to "/posts"
+    user_email = params["user"]["email"] 
+    puts params
+    existing_user = User.find_by({"email"=>user_email})
+    if existing_user
+      #user email is in database
+      #redirect to login page
+      #flash notice "user email is taken"
+      flash["notice"] = "user email is taken"
+      redirect_to "/login"
+    
+    else 
+      @user = User.new
+      @user["first_name"] = params["user"]["first_name"]
+      @user["last_name"] = params["user"]["last_name"]
+      @user["email"] = params["user"]["email"]
+      @user["password"] = BCrypt::Password.create(params["user"]["password"])
+      @user.save
+      redirect_to "/posts"
+    
+    
+    end 
+
   end
 end
 
